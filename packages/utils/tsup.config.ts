d8f8f700e44e66b82c01b2dcccf26bc59b41f49f
Nikos Kontakis <wirednkod@gmail.com>
2023-11-28 23:46:07 +0200

    Alter build method for @polkadot-cloud/utils from rollup to tsup (#51)

---
+++ packages/utils/tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["lib/index.ts"],
+  splitting: false,
+  sourcemap: false,
+  clean: true,
+  dts: true,
+  format: ["cjs", "esm"],
+});
