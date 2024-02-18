25d8d602687b450119fc862a25368fe8a20ca04e
Ross Bulat <ross@parity.io>
2024-02-18 18:53:29 +0700

    chore: remove index styling

--- sandbox/src/main.tsx
+++ sandbox/src/main.tsx
@@ -4,12 +4,12 @@ SPDX-License-Identifier: GPL-3.0-only */
 import * as ReactDOM from "react-dom/client";
 import { App } from "./App";
 
+import "../../packages/cloud-core/dist/theme/default/index.css";
+
 import "./theme/fonts.css";
 import "./theme/theme.css";
-import "../../packages/cloud-core/dist/theme/default/index.css";
-import "../../packages/cloud-core/dist/css/styles/index.css";
-
 import "./styles/index.scss";
+import "./styles/app.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <App />
